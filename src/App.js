9bb78e320d9fb47ceb83c226e8967e0fb9b5bfff
jjjunio <jjjunio@gmail.com>
2020-08-18 12:02:04 -0400

    adding country picker and chart component functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { fetchData } from "./api";
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
@@ -14,13 +15,24 @@ class App extends React.Component {
 
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+    console.log(fetchedData);
+    // console.log(country);
+    //fetch data
+    //set state
+  };
+
   render() {
     const { data } = this.state;
 
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
